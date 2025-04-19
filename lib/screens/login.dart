81ae94389dce5ecd887bb834ec44bc5c635c7aa1
Vick Durazzo <marividurazzo007@gmail.com>
2025-04-18 23:13:40 -0300

    feat: create login screen

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,74 +1,128 @@
 import 'package:flutter/material.dart';
-import '../widgets/primary_button.dart'; // ajuste o caminho se necessário
+import '../widgets/primary_button.dart';
+import '../widgets/form_input.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       backgroundColor:  const Color(0xFF1B1A3B),
-       body: Stack(
+      backgroundColor:  const Color(0xFF1B1A3B),
+      body: Stack(
         fit: StackFit.expand,
         children: <Widget>[
           Image.asset(
             'assets/background.png',
             fit: BoxFit.cover,
           ),
+          Positioned(
+            top: 20,
+            left: 20,
+            child: Image.asset(
+              'assets/logo.png',
+              height: 50,
+            ),
+          ),
+          Positioned(
+            top: 0,
+            right: 0,
+            child: Image.asset(
+              'assets/planet1.png',
+              height: 200,
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: Image.asset(
+              'assets/planet2.png',
+              height: 150,
+            ),
+          ),
           SafeArea(
             child: Padding(
               padding: const EdgeInsets.all(20.0),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-                  Align(
-                    alignment: Alignment.topRight,
-                    child: Image.asset(
-                      'assets/logo.png',
-                      height: 50,
-                    ),
-                  ),
-                  Column(
+                Container(
+                  width: double.infinity,
+                  height: 190,
+                   child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Text(
-                        'Bem-vindos à missão Laica',
-                        textAlign: TextAlign.center,
+                      Align(
+                        child: Text(
+                        'Exploradores',
                         style: TextStyle(
                           color: Colors.white,
                           fontSize: 30,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                      SizedBox(height: 10),
-                      Text(
-                        'Prontos para embarcar no foguete ?',
-                        textAlign: TextAlign.center,
+                      ),
+                      Align(
+                       child: Text(
+                        'Acessem sua aventuras agora !',
                         style: TextStyle(
                           color: Colors.white,
                           fontSize: 18,
                         ),
                       ),
+                      ),
                     ],
                   ),
-                  PrimaryButton(text: 'Embarcar',  onPressed: () {
-                    Navigator.pushNamed(context, '/login');
-                  }),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushNamed(context, '/register');
-                    },
-                    child: Text(
-                      'Novos Astronautas ? Cadastrem-se',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        decoration: TextDecoration.underline, // Optional: Add underline for better visual cue
+                ),
+                  
+                  CustomTextField(
+                    controller: _emailController,
+                    labelText: 'E-mail',
+                    suffixIcon: Icons.email,
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  CustomTextField(
+                    controller: _passwordController,
+                    labelText: 'Senha',
+                    suffixIcon: Icons.lock_outline,
+                    keyboardType: TextInputType.visiblePassword,
+
+
+                    ),
+                    PrimaryButton(
+                      text: 'Decolar',
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/menu');
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, '/register');
+                      },
+                      child: const Text(
+                        'Nova astronauta ? Cadastre-sem',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
                       ),
                     ),
-
-                  ),
                 ],
               ),
             ),
---
+++ lib/widgets/form_input.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String labelText;
+  final IconData suffixIcon;
+  final TextInputType keyboardType;
+
+  const CustomTextField({
+    Key? key,
+    required this.controller,
+    required this.labelText,
+    required this.suffixIcon,
+    this.keyboardType = TextInputType.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: TextField(
+        controller: controller,
+        keyboardType: keyboardType,
+        decoration: InputDecoration(
+          labelText: labelText,
+          labelStyle: const TextStyle(color: Colors.grey),
+          filled: true,
+          fillColor: Colors.white,
+          suffixIcon: Icon(suffixIcon, color: Colors.grey),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
